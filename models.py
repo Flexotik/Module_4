8f8e718cd5725457eaa58ab42f81de7ad60966b3
Alina Chernikova <154429660+Flexotik@users.noreply.github.com>
2026-05-21 13:34:50 +0800

    Добавлен класс Book в models.py

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
+from dataclasses import dataclass
+from datetime import datetime
 
+@dataclass
+class Book:
+    author: str
+    title: str
+    rating: int  
+    read_date: str
+
+    def to_dict(self):
+        return {
+            "author": self.author,
+            "title": self.title,
+            "rating": self.rating,
+            "read_date": self.read_date
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(
+            author=data["author"],
+            title=data["title"],
+            rating=data["rating"],
+            read_date=data["read_date"]
+        )
